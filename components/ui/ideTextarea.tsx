6e4a1010c29eb0dbf6fb2c8e794d2c7b40e0ff5e
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2023-05-23 14:32:28 -0700

    fix suggestions getter

--- components/ui/ideTextarea.tsx
+++ components/ui/ideTextarea.tsx
@@ -12,16 +12,11 @@ export default function IdeTextarea(props: any) {
         if (event.key == "Tab") {
             event.preventDefault();
             const textArea = event.target as HTMLTextAreaElement;
-            const start = textArea.selectionStart;
-            const end = textArea.selectionEnd;
-            const text = textArea.value;
-            const word = text.substring(start, end);
+            const start = findStartOfLastWord(textArea.value);
+            const end = textArea.textLength;
+            const word = textArea.value.substring(start, end);
             const suggestions = getSuggestions(word, props.functionNames);
-            if (suggestions.length == 1) {
-                textArea.value = text.substring(0, start) + suggestions[0] + text.substring(end);
-                textArea.selectionStart = start + suggestions[0].length;
-                textArea.selectionEnd = start + suggestions[0].length;
-            }
+            console.log(suggestions);
         }
     };
 
@@ -30,6 +25,14 @@ export default function IdeTextarea(props: any) {
             <textarea className="bg-gray-800 text-white w-full h-screen" onKeyDown={handleKeyDown} />
         </div>
     );
+}
+
+function findStartOfLastWord(text: string) {
+    let i = text.length - 1;
+    while (i >= 0 && text[i] != " ") {
+        i--;
+    }
+    return i + 1;
 }
 
 function getSuggestions(word: string, functionNames: string[]): string[] {
