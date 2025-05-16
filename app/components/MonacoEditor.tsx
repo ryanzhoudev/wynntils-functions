53ed2e317dfc8b7d4eda9ff2aaa3e92d998c1c6f
Ryan Zhou <57310593+ryanzhoudev@users.noreply.github.com>
2025-05-16 16:55:11 -0700

    attempt copy bare example

--- app/components/MonacoEditor.tsx
+++ app/components/MonacoEditor.tsx
@@ -1,23 +1,67 @@
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import * as monaco from "monaco-editor";
+import { ConsoleLogger } from "monaco-languageclient/tools";
+import { LogLevel } from "@codingame/monaco-vscode-api";
+import { initServices } from "monaco-languageclient/vscode/services";
+import { configureDefaultWorkerFactory } from "monaco-editor-wrapper/workers/workerLoaders";
+import { LanguageClientWrapper } from "monaco-editor-wrapper";
+import type { LanguageClientConfig } from "monaco-editor-wrapper";
 
-const MonacoEditor = () => {
+const MonacoEditor = async () => {
     const containerRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        if (!containerRef.current) return;
+    const logger = new ConsoleLogger(LogLevel.Debug);
+    await initServices(
+        {
+            serviceOverrides: {
+                // none
+            },
+            userConfiguration: {
+                json: JSON.stringify({
+                    "editor.experimental.asyncTokenization": true
+                })
+            }
+        },
+        {
+            logger
+        }
+    );
 
-        const editor = monaco.editor.create(containerRef.current, {
-            value: "// Start scripting in Wynntils...\n",
-            language: "javascript", // placeholder for now
-            theme: "vs-dark",
-            automaticLayout: true
-        });
+    monaco.languages.register({
+        id: "wynntils"
+    });
 
-        return () => editor.dispose();
-    }, []);
+    configureDefaultWorkerFactory(logger);
 
-    return <div ref={containerRef} className="w-full h-screen" />;
+    if (!containerRef.current) return <p>containerRef.current is null</p>;
+    monaco.editor.create(containerRef.current, {
+        value: "Wynntils scripting goes here",
+        language: "wynntils",
+        theme: "vs-dark",
+        automaticLayout: true,
+        wordBasedSuggestions: "off"
+    });
+
+    const languageClientConfig: LanguageClientConfig = {
+        clientOptions: {
+            documentSelector: ["wynntils"]
+        },
+        connection: {
+            options: {
+                $type: "WebSocketUrl",
+                url: "ws://localhost:30000/sampleServer"
+            }
+        }
+    };
+
+    const languageClientWrapper = new LanguageClientWrapper({
+        languageClientConfig,
+        logger
+    });
+
+    await languageClientWrapper.start();
+
+    return <div ref={containerRef} className="h-screen w-full" />;
 };
 
 export default MonacoEditor;
